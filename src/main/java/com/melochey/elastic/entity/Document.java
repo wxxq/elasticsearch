d89f4749a9aaae6df0f31afad88a5dfa2e209a07
Leonboo <denny1011@163.com>
2018-02-26 17:05:52 +0800

    add province city money

--- src/main/java/com/melochey/elastic/entity/Document.java
+++ src/main/java/com/melochey/elastic/entity/Document.java
@@ -7,6 +7,12 @@ public class Document {
     private String message;
     private int age;
     private int height;
+	private String category;
+	private String school;
+    private int province_id;
+    private int city_id;
+    private double money;
+
 	public String getFirstname() {
 		return firstname;
 	}
@@ -42,5 +48,35 @@ public class Document {
 	}
 	public void setAge(int age) {
 		this.age = age;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public double getMoney() {
+		return money;
+	}
+	public void setMoney(double money) {
+		this.money = money;
+	}
+	public int getProvince_id() {
+		return province_id;
+	}
+	public void setProvince_id(int province_id) {
+		this.province_id = province_id;
+	}
+	public int getCity_id() {
+		return city_id;
+	}
+	public void setCity_id(int city_id) {
+		this.city_id = city_id;
 	}
 }
